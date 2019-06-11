efc32764639c5156af50afe8dc7d78b8e6716eae
cizezsy <zsy19980307@gmail.com>
2019-06-11 17:27:09 +0800

    Fix a problem when ca is not specified will cause runtime exception

--- src/test/java/io/alauda/jenkins/devops/support/KubernetesClusterConfigurationTest.java
+++ src/test/java/io/alauda/jenkins/devops/support/KubernetesClusterConfigurationTest.java
@@ -1,5 +1,6 @@
 package io.alauda.jenkins.devops.support;
 
+import com.gargoylesoftware.htmlunit.html.HtmlCheckBoxInput;
 import com.gargoylesoftware.htmlunit.html.HtmlForm;
 import com.gargoylesoftware.htmlunit.html.HtmlTextInput;
 import org.junit.Test;
@@ -19,12 +20,12 @@ public class KubernetesClusterConfigurationTest {
             HtmlForm config = r.createWebClient().goTo("configure").getFormByName("config");
             HtmlTextInput textbox = config.getInputByName("_.masterUrl");
             textbox.setText("http://test");
+            HtmlCheckBoxInput checkBoxInput = config.getInputByName("_.skipTlsVerify");
+            checkBoxInput.setChecked(false);
             r.submit(config);
             assertEquals("global config page let us edit it", "http://test", KubernetesClusterConfiguration.get().getCluster().getMasterUrl());
         });
-        rr.then(r -> {
-            assertEquals("still there after restart of Jenkins", "http://test", KubernetesClusterConfiguration.get().getCluster().getMasterUrl());
-        });
+        rr.then(r -> assertEquals("still there after restart of Jenkins", "http://test", KubernetesClusterConfiguration.get().getCluster().getMasterUrl()));
     }
 
 }
